9ff0b6c44d260db3fb73856545d525040ce9bdab
Sylvain Floury <sylvain@floury.name>
2014-11-13 11:23:17 +0100

    Test fonctionnel d'ajout d'un contact.

--- Bundle/TddDemoBundle/Tests/Controller/DefaultControllerTest.php
+++ Bundle/TddDemoBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,23 @@ class DefaultControllerTest extends WebTestCase
     public function testContact()
     {
         $client = static::createClient();
+        $client->followRedirects();
 
-        $crawler = $client->request('GET', '/contact');
+        // Appel de l'URL de l'application
+        $crawler = $client->request('GET', '/cvtic/contact');
         
+        // Contrôle de l'URL
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+
+        // Complete le formulaire et l'envoie
+        $form = $crawler->selectButton('Ajouter')->form();
+        
+        $form['contact[name]'] = 'Sylvain';
+        $form['contact[email]'] = 'sylvain@floury.name';
+        
+        $client->submit($form);
+        
+        // Contrôle du message de confirmation
+        $this->assertRegExp('/Ajout du contact : Sylvain - sylvain@floury.name/', $client->getResponse()->getContent());
     }
 }
